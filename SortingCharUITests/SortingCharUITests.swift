948ae48bc188964a7cace8d226850ad38aba4230
Olarn U <olarn.u@gmail.com>
2016-07-30 15:46:15 +0700

    backup ก่อน run Swiftlint

--- SortingCharUITests/SortingCharUITests.swift
+++ SortingCharUITests/SortingCharUITests.swift
@@ -12,41 +12,29 @@ class SortingCharUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testDisplaySortedCharWhenClickSort() {
         let app = XCUIApplication()
         
         app.textFields["inputField"].tap()
         app.textFields["inputField"].typeText("zad")
-        
         app.buttons["Sorting"].tap()
-        
         XCTAssertTrue(app.staticTexts["SortedCharText"].label == "adz")
     }
-    
+
     func testDisplayBlankWhenCharIsEmpty() {
         let app = XCUIApplication()
-        
         app.textFields["inputField"].tap()
         app.textFields["inputField"].typeText("")
-        
         app.buttons["Sorting"].tap()
-        
-        XCTAssertTrue(app.staticTexts["SortedCharText"].label == "Please enter text before call sort.")
+        let expectError = "Please enter text before call sort."
+        XCTAssertTrue(app.staticTexts["SortedCharText"].label == expectError)
     }
 }

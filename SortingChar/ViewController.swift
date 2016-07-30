6bb36560190c076abade6fb092fbba78448e443f
Olarn U <olarn.u@gmail.com>
2016-07-30 10:43:57 +0700

    Initial Commit

---
+++ SortingChar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SortingChar
+//
+//  Created by Olarn U. on 7/30/2559 BE.
+//  Copyright © 2559 Olarn U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
